5fe9223355a9b82cfafc185e485e696cdf7e98eb
Angel L. P <59593766+angelphanth@users.noreply.github.com>
2024-10-08 13:40:26 +0200

    modify run_search to query as module and script :sparkles:

--- run_search.py
+++ run_search.py
@@ -18,6 +18,66 @@ from pubmed_rag.model import (
     get_sentence_embeddings
 )
 
+def main(
+        path_to_config:str,
+        query:str,
+        logging:bool=False
+):
+    # allow logging if ran as script
+    if logging:
+        verbose = logger.info
+    else:
+        verbose = print
+
+    ## LOAD CONFIG PARAMETERS
+    # getting the config filepath
+    config_filepath = path_to_config
+    # log it
+    verbose(f'Path to config file: {config_filepath}')
+    # load config params
+    verbose("Loading config params ... ")
+    config = config_loader(config_filepath)
+    assert_nonempty_keys(config)
+    assert_nonempty_vals(config)
+    output_path = config['biocjson output path']
+    chosen_model = config['transformer_model']
+    db_name = config['db name']
+    n_results = config['n_results']
+    verbose(f'Configuration: {config}')
+
+    ## MAIN
+    verbose(f"Accessing {db_name}")
+    client = MilvusClient(db_name)
+
+    verbose(f"Loading model {chosen_model} from HuggingFace")
+    tokenizer = AutoTokenizer.from_pretrained(chosen_model)
+    model = AutoModel.from_pretrained(chosen_model)
+
+    # GET EMBEDDINGS
+    verbose(f"Embedding query: {query}")
+    # Tokenize sentences
+    encoded_input = get_tokens(
+        tokenizer,
+        [query]
+    )
+    # get embeddings
+    sentence_embeddings = get_sentence_embeddings(
+        model,
+        encoded_input
+    )           
+
+    ## RUN QUERY
+    verbose('Searching against vector database')
+    result = client.search(
+        collection_name="main",
+        data=sentence_embeddings.detach().numpy(),
+        #filter="section == 'abstract'",
+        limit=n_results,
+        output_fields=["text", "pmid", 'section'],
+    )
+
+    return result[0]
+
 if __name__ == "__main__":
 
     ## GET ARGS
@@ -47,59 +107,19 @@ if __name__ == "__main__":
     # log it
     logger.info(f'Path to log file: {log_file}')
 
-    ## LOAD CONFIG PARAMETERS
-    # getting the config filepath
-    config_filepath = args.config
-    # log it
-    logger.info(f'Path to config file: {config_filepath}')
-    # load config params
-    logger.info("Loading config params ... ")
-    config = config_loader(config_filepath)
-    assert_nonempty_keys(config)
-    assert_nonempty_vals(config)
-    output_path = config['biocjson output path']
-    chosen_model = config['transformer_model']
-    db_name = config['db name']
-    n_results = config['n_results']
-    logger.info(f'Configuration: {config}')
-
-    ## MAIN
-    logger.info(f"Accessing {db_name}")
-    client = MilvusClient(db_name)
-
-    logger.info(f"Loading model {chosen_model} from HuggingFace")
-    tokenizer = AutoTokenizer.from_pretrained(chosen_model)
-    model = AutoModel.from_pretrained(chosen_model)
-
-    # GET EMBEDDINGS
-    logger.info(f"Embedding query: {args.query}")
-    # Tokenize sentences
-    encoded_input = get_tokens(
-        tokenizer,
-        [args.query]
-    )
-    # get embeddings
-    sentence_embeddings = get_sentence_embeddings(
-        model,
-        encoded_input
-    )           
-
-    ## RUN QUERY
-    logger.info('Searching against vector database')
-    result = client.search(
-        collection_name="main",
-        data=sentence_embeddings.detach().numpy(),
-        #filter="section == 'abstract'",
-        limit=n_results,
-        output_fields=["text", "pmid", 'section'],
+    result = main(
+        path_to_config=args.config,
+        query=args.query,
+        logging=True, 
     )
 
     logger.info(f"Query: {args.query}")
     logger.info(f"Results:")
-    for x in (result[0]):
+    for x in (result):
         logger.info(json.dumps(x, indent=4))
 
     logger.info('Complete.')
 
 else:
-    print('run_query.py imported. Script not ran.')
+    print('Warning: run_query.py imported as a module. Script not ran.')
+
