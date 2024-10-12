bca9d771c9a42868fc5aa9ec7663ab9014d8ac28
Angel L. P <59593766+angelphanth@users.noreply.github.com>
2024-10-12 19:55:16 +0200

    :cake: used black

--- pubmed_rag/utils.py
+++ pubmed_rag/utils.py
@@ -7,11 +7,12 @@ import logging
 import glob
 import warnings
 
+
 ## CHECKS
-def assert_path(filepath:str):
+def assert_path(filepath: str):
     """
     Checks that fpath is a string and that it exists
-    
+
     PARAMS
     -----
     - filepath (str): the filepath or folderpath
@@ -21,15 +22,16 @@ def assert_path(filepath:str):
     - raises assertion error if filepath is not a string or doesn't exist
     """
 
-    assert isinstance(filepath, str), \
-        f'filepath must be a string: {filepath}'
-    assert os.path.exists(os.path.abspath(filepath)), \
-        f'filepath does not exist: {os.path.abspath(filepath)}'
+    assert isinstance(filepath, str), f"filepath must be a string: {filepath}"
+    assert os.path.exists(
+        os.path.abspath(filepath)
+    ), f"filepath does not exist: {os.path.abspath(filepath)}"
 
-def assert_nonempty_keys(dictionary:dict):
+
+def assert_nonempty_keys(dictionary: dict):
     """
     - Checks that the keys are not empty strings
-    - Can they be numbers? I guess 
+    - Can they be numbers? I guess
 
     PARAMS
     -----
@@ -37,15 +39,18 @@ def assert_nonempty_keys(dictionary:dict):
     """
 
     # PRECONDITIONS
-    assert isinstance(dictionary, dict), 'dictionary must be a dict'
+    assert isinstance(dictionary, dict), "dictionary must be a dict"
 
     # MAIN FUNCTION
     for key in dictionary:
         if type(key) is str:
             assert key, f'There is an empty key (e.g., ""): {key, dictionary.keys()}'
-            assert key.strip(), f'There is a blank key (e.g., space, " "): {key, dictionary.keys()}'
+            assert (
+                key.strip()
+            ), f'There is a blank key (e.g., space, " "): {key, dictionary.keys()}'
 
-def assert_nonempty_vals(dictionary:dict):
+
+def assert_nonempty_vals(dictionary: dict):
     """
     - Checks that the dict values are not empty strings
 
@@ -55,20 +60,23 @@ def assert_nonempty_vals(dictionary:dict):
     """
 
     # PRECONDITIONS
-    assert isinstance(dictionary, dict), 'dictionary must be a dict'
+    assert isinstance(dictionary, dict), "dictionary must be a dict"
 
     # MAIN FUNCTION
     for v in dictionary.items():
         if type(v) is str:
             assert v, f'There is an empty key (e.g., ""): {v, dictionary.items()}'
-            assert v.strip(), f'There is a blank key (e.g., space, " "): {v, dictionary.items()}'
+            assert (
+                v.strip()
+            ), f'There is a blank key (e.g., space, " "): {v, dictionary.items()}'
+
 
 def warn_folder(
-        folderpath:str, 
-        warning_message:str=f'Warning: There are existing files in the given folderpath.'
-    ):
+    folderpath: str,
+    warning_message: str = f"Warning: There are existing files in the given folderpath.",
+):
     """
-    Checks if the folder is empty. If not empty raise a warning. 
+    Checks if the folder is empty. If not empty raise a warning.
 
     PARAMETERS
     -----
@@ -84,7 +92,7 @@ def warn_folder(
     -----
     1)
     >>> warn_folder('/Users/this/that/empty_folder')
-    
+
     2)
     >>> warn_folder('/Users/this/that/non_empty_folder')
     Warning: There are existing files in the given folderpath.
@@ -92,18 +100,18 @@ def warn_folder(
 
     # PRECONDITIONS
     assert_path(folderpath)
-    assert isinstance(warning_message, str), f'warning message must be string: {warning_message}'
+    assert isinstance(
+        warning_message, str
+    ), f"warning message must be string: {warning_message}"
 
-    if len(glob.glob(os.path.join(folderpath,'*'))) > 0: 
-        warnings.warn(
-            warning_message,
-            UserWarning
-        )
+    if len(glob.glob(os.path.join(folderpath, "*"))) > 0:
+        warnings.warn(warning_message, UserWarning)
 
         return warning_message
 
-# FUNCTIONS FOR CONFIG 
-def config_loader(filepath:str) -> dict:
+
+# FUNCTIONS FOR CONFIG
+def config_loader(filepath: str) -> dict:
     """
     Loads in yaml config file as dict
 
@@ -118,14 +126,14 @@ def config_loader(filepath:str) -> dict:
     EXAMPLE
     -----
     >>> config_dict = config_loader('config/config.yaml')
-    
+
     """
 
     # PRECONDITIONS
     assert_path(filepath)
 
     # MAIN FUNCTION
-    with open(filepath, 'r') as f:
+    with open(filepath, "r") as f:
         contents = yaml.safe_load(f)
 
     # POSTCONDITIONS
@@ -133,60 +141,56 @@ def config_loader(filepath:str) -> dict:
 
     return contents
 
-def get_args(prog_name:str, others:dict={}):
+
+def get_args(prog_name: str, others: dict = {}):
     """
     Initiates argparse.ArugmentParser() and adds common arguments.
 
-    :param prog_name: The name of the program. 
+    :param prog_name: The name of the program.
     :type prog_name: str
 
-    :returns: 
-    :rtype: 
+    :returns:
+    :rtype:
     """
 
     ### PRECONDITIONS
-    assert isinstance(prog_name, str), \
-        f"prog_name should be a string: {prog_name}"
-    assert isinstance(others, dict), \
-        f"other kwargs must be a dict: {others}"
+    assert isinstance(prog_name, str), f"prog_name should be a string: {prog_name}"
+    assert isinstance(others, dict), f"other kwargs must be a dict: {others}"
     ## MAIN FUNCTION
     # init
-    parser = argparse.ArgumentParser(
-        prog=prog_name,
-        **others
-    )
+    parser = argparse.ArgumentParser(prog=prog_name, **others)
     # config file path
     parser.add_argument(
-        '-c', '--config',
-        action='store',
-        default='demo/config.yaml',
-        help='provide path to config yaml file'
+        "-c",
+        "--config",
+        action="store",
+        default="demo/config.yaml",
+        help="provide path to config yaml file",
     )
     # used in run_search.py
     parser.add_argument(
-        '-q', '--query',
-        action='store',
-        help='text to embed and search in vector db'
+        "-q", "--query", action="store", help="text to embed and search in vector db"
     )
     # used in get_embeddings.py
     parser.add_argument(
-        '-fd', '--files_downloaded',
-        action='store_true',
-        help='add this flag if the biocjson files already exist in the "pmid file path" in the config yaml'
+        "-fd",
+        "--files_downloaded",
+        action="store_true",
+        help='add this flag if the biocjson files already exist in the "pmid file path" in the config yaml',
     )
     args = parser.parse_args()
     return args
 
 
 ## FOR LOGGING
-def get_basename(fname:None|str=None)->str:
+def get_basename(fname: None | str = None) -> str:
     """
     - For a given filename, returns basename WITHOUT file extension
     - If no fname given (i.e., None) then return basename that the function is called in
 
     PARAMS
     -----
-    - fname (None or str): the filename to get basename of, or None 
+    - fname (None or str): the filename to get basename of, or None
 
     OUTPUTS
     -----
@@ -198,11 +202,11 @@ def get_basename(fname:None|str=None)->str:
     >>> get_basename()
     utils
 
-    2) 
+    2)
     >>> get_basename('this/is-a-filepath.csv')
     is-a-filepath
     """
-    if fname is not None: 
+    if fname is not None:
         # PRECONDITION
         assert_path(fname)
         # MAIN FUNCTIONS
@@ -210,7 +214,8 @@ def get_basename(fname:None|str=None)->str:
     else:
         return os.path.splitext(os.path.basename(sys.argv[0]))[0]
 
-def get_time(incl_time:bool=True, incl_timezone:bool=True) -> str:
+
+def get_time(incl_time: bool = True, incl_timezone: bool = True) -> str:
     """
     Gets current date, time (optional) and timezone (optional) for file naming
 
@@ -221,8 +226,8 @@ def get_time(incl_time:bool=True, incl_timezone:bool=True) -> str:
 
     RETURNS
     -----
-    - fname (str): includes date, timestamp and/or timezone 
-        connected by '_' in one string e.g. yyyyMMdd_hhmm_timezone 
+    - fname (str): includes date, timestamp and/or timezone
+        connected by '_' in one string e.g. yyyyMMdd_hhmm_timezone
 
     EXAMPLES
     -----
@@ -237,8 +242,8 @@ def get_time(incl_time:bool=True, incl_timezone:bool=True) -> str:
     """
 
     # PRECONDITIONALS
-    assert isinstance(incl_time, bool), 'incl_time must be True or False'
-    assert isinstance(incl_timezone, bool), 'incl_timezone must be True or False'
+    assert isinstance(incl_time, bool), "incl_time must be True or False"
+    assert isinstance(incl_timezone, bool), "incl_timezone must be True or False"
 
     # MAIN FUNCTION
     # getting current time and timezone
@@ -253,26 +258,27 @@ def get_time(incl_time:bool=True, incl_timezone:bool=True) -> str:
     s = str(the_time.second)
     # putting date parts into one string
     if incl_time and incl_timezone:
-        fname = '_'.join([y+M+d, h+m+s, timezone])
+        fname = "_".join([y + M + d, h + m + s, timezone])
     elif incl_time:
-        fname = '_'.join([y+M+d, h+m+s])
+        fname = "_".join([y + M + d, h + m + s])
     elif incl_timezone:
-        fname = '_'.join([y+M+d, timezone])
+        fname = "_".join([y + M + d, timezone])
     else:
-        fname = y+M+d
+        fname = y + M + d
 
     # POSTCONDITIONALS
-    parts = fname.split('_')
+    parts = fname.split("_")
     if incl_time and incl_timezone:
-        assert len(parts) == 3, f'time and/or timezone inclusion issue: {fname}'
+        assert len(parts) == 3, f"time and/or timezone inclusion issue: {fname}"
     elif incl_time or incl_timezone:
-        assert len(parts) == 2, f'time/timezone inclusion issue: {fname}'
+        assert len(parts) == 2, f"time/timezone inclusion issue: {fname}"
     else:
-        assert len(parts) == 1, f'time/timezone inclusion issue: {fname}'
+        assert len(parts) == 1, f"time/timezone inclusion issue: {fname}"
 
     return fname
 
-def generate_log_filename(folder:str='logs', suffix:str='') -> str:
+
+def generate_log_filename(folder: str = "logs", suffix: str = "") -> str:
     """
     Creates log file name and path
 
@@ -287,26 +293,27 @@ def generate_log_filename(folder:str='logs', suffix:str='') -> str:
     """
     # PRECONDITIONS
     assert_path(folder)
-    
+
     # MAIN FUNCTION
-    log_filename = get_time(incl_timezone=False) + '_' + suffix +'.log'
+    log_filename = get_time(incl_timezone=False) + "_" + suffix + ".log"
     log_filepath = os.path.join(folder, log_filename)
-    
+
     return log_filepath
 
-def init_log(filename:str, display:bool=False, logger_id:str|None=None):
+
+def init_log(filename: str, display: bool = False, logger_id: str | None = None):
     """
     - Custom python logger configuration (basicConfig())
         with two handlers (for stdout and for file)
     - from: https://stackoverflow.com/a/44760039
     - Keeps a log record file of the python application, with option to
-        display in stdout 
-    
+        display in stdout
+
     PARAMETERS
     -----
     - filename (str): filepath to log record file
     - display (bool): whether to print the logs to whatever standard output
-    - logger_id (str): an optional identifier for yourself, 
+    - logger_id (str): an optional identifier for yourself,
         if None then defaults to 'root'
 
     RETURNS
@@ -320,26 +327,27 @@ def init_log(filename:str, display:bool=False, logger_id:str|None=None):
     [2023-10-20 10:38:03,074] root: INFO - Loading things
     """
     # PRECONDITIONALS
-    assert isinstance(filename, str), 'filename must be a string'
-    assert (isinstance(logger_id, str) or logger_id is None), \
-        'logger_id must be a string or None'
+    assert isinstance(filename, str), "filename must be a string"
+    assert (
+        isinstance(logger_id, str) or logger_id is None
+    ), "logger_id must be a string or None"
 
     # MAIN FUNCTION
     # init handlers
     file_handler = logging.FileHandler(filename=filename)
     stdout_handler = logging.StreamHandler(stream=sys.stdout)
-    if display: 
+    if display:
         handlers = [file_handler, stdout_handler]
-    else: 
+    else:
         handlers = [file_handler]
 
     # logger configuration
     logging.basicConfig(
-        #level=logging.DEBUG, 
-        format='[%(asctime)s] %(name)s: %(levelname)s - %(message)s',
-        handlers=handlers
+        # level=logging.DEBUG,
+        format="[%(asctime)s] %(name)s: %(levelname)s - %(message)s",
+        handlers=handlers,
     )
-    logging.getLogger('matplotlib.font_manager').disabled = True
+    logging.getLogger("matplotlib.font_manager").disabled = True
 
     # instantiate the logger
     logger = logging.getLogger(logger_id)
@@ -355,81 +363,87 @@ def get_chunks(lst, n):
     https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
     """
     for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+        yield lst[i : i + n]
+
 
 def filter_filepaths(
-        fpath:str|list, 
-        identifiers:list=[''], 
-        exclude:None|list=None
-    )->list:
+    fpath: str | list, identifiers: list = [""], exclude: None | list = None
+) -> list:
     """
     Isolating files to iterate through. Can provide multiple identifiers.
-    if list given, then filters list. 
-    if str/path given, then acquires list first. 
+    if list given, then filters list.
+    if str/path given, then acquires list first.
     """
 
     # PRECONDITIONALS
-    assert isinstance(identifiers, list), 'exclude must be None or a list of strngs'
+    assert isinstance(identifiers, list), "exclude must be None or a list of strngs"
     for id in identifiers:
-        assert isinstance(id, str), f'must all be strings: {id} not a string'
-       
+        assert isinstance(id, str), f"must all be strings: {id} not a string"
+
     if exclude is not None:
-        assert isinstance(exclude, list), 'exclude must be None or a list of strngs'
+        assert isinstance(exclude, list), "exclude must be None or a list of strngs"
         for ex in exclude:
-            assert isinstance(ex, str), f'must all be strings: {ex} not a string'
-    
+            assert isinstance(ex, str), f"must all be strings: {ex} not a string"
+
     # MAIN FUNCTION
 
     # if path (str) given then get a list of files first
     if type(fpath) is str:
         assert_path(fpath)
-        filepaths = glob.glob(f'{fpath}/**', recursive=True)
+        filepaths = glob.glob(f"{fpath}/**", recursive=True)
     # if a list of filenames then continue
     elif type(fpath) is list:
         filepaths = fpath
-    else: 
-        raise TypeError(f'fpath must be a string or list: {type(fpath)}')
+    else:
+        raise TypeError(f"fpath must be a string or list: {type(fpath)}")
 
     # check
     for path in filepaths:
         assert_path(path)
 
     # filtering for files that have those identifiers
-    in_filtered = [file for file in filepaths if \
-                all([id in os.path.basename(file) for id in identifiers])]
-    
+    in_filtered = [
+        file
+        for file in filepaths
+        if all([id in os.path.basename(file) for id in identifiers])
+    ]
+
     if exclude is None:
         return in_filtered
     else:
         # filter out the files that match in the exclusion list
-        ex_filtered = [file for file in in_filtered if \
-                    all([ex not in os.path.basename(file) for ex in exclude])]
+        ex_filtered = [
+            file
+            for file in in_filtered
+            if all([ex not in os.path.basename(file) for ex in exclude])
+        ]
 
         return ex_filtered
-    
-def pipe(input, functions:list=[]):
-    '''
+
+
+def pipe(input, functions: list = []):
+    """
     - Pipes output of one function into another like '|' in linux
     - https://stackoverflow.com/questions/28252585/functional-pipes-in-python-like-from-rs-magrittr
-    - Limitation: 
+    - Limitation:
         - All functions must only expect one argument
         - Defaults will have to be used for all other arguments
-    
+
     PARAMS
     -----
     - input (Any): whatever input the first function is expecting
-    - functions (list): a list of functions IN ORDER OF EXECUTION 
+    - functions (list): a list of functions IN ORDER OF EXECUTION
         do not include () at end of functions
 
     OUTPUTS
     -----
     - Any or None (whatever the expected output is from the final function in the list)
 
-    '''
+    """
     # PRECONDITIONALS
-    assert isinstance(functions, list), 'functions should be in a list'
-    for f in functions: 
-        assert callable(f), f'{f} is not a function'
+    assert isinstance(functions, list), "functions should be in a list"
+    for f in functions:
+        assert callable(f), f"{f} is not a function"
 
     # MAIN FUNCTION
     for f in functions:
