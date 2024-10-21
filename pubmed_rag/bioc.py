5cedbcc624ce06cc37eb9138276da2880766f595
Angel L. P <59593766+angelphanth@users.noreply.github.com>
2024-10-21 18:43:59 +0200

    :art: ruff formatting

--- pubmed_rag/bioc.py
+++ pubmed_rag/bioc.py
@@ -1,15 +1,23 @@
 # imports
-import os, time, json, requests 
-from pubmed_rag.utils import assert_path, get_chunks
-import pandas as pd
-import torch
+import json
+import os
+import time
 
 import nltk
-nltk.download('punkt_tab')
+import pandas as pd
+import requests
+import torch
+
+from pubmed_rag.utils import assert_path, get_chunks
+
+nltk.download("punkt_tab")
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 
-def get_biocjson(id:str, out_path:str, prefix:str='biocjson_', wait:int|float=0)->dict:
+
+def get_biocjson(
+    id: str, out_path: str, prefix: str = "biocjson_", wait: int | float = 0
+) -> dict:
     """
     Retrieves full text (if available) or abstract only from PubMed/Central given a PMID or PMCID.
 
@@ -22,7 +30,7 @@ def get_biocjson(id:str, out_path:str, prefix:str='biocjson_', wait:int|float=0)
     :param wait: The number of seconds to wait between each request.
     :type wait: int
 
-    :returns: 
+    :returns:
         - JSON files saved to the specified out_path.
         - A dictionary where keys are the PMID/PMCID and values are BioC JSON data.
     :rtype: dict
@@ -31,9 +39,10 @@ def get_biocjson(id:str, out_path:str, prefix:str='biocjson_', wait:int|float=0)
     ### PRECONDITIONS
     assert isinstance(id, str), f"id must be a str: {id}"
     assert_path(out_path)
-    assert isinstance(prefix, str), f'prefix must be a string: {prefix}'
-    assert (isinstance(wait, int) | isinstance(wait, float)),\
-        f"wait must be an integer or float: {wait}"
+    assert isinstance(prefix, str), f"prefix must be a string: {prefix}"
+    assert isinstance(wait, int) | isinstance(
+        wait, float
+    ), f"wait must be an integer or float: {wait}"
 
     ### MAIN FUNCTION
 
@@ -46,23 +55,25 @@ def get_biocjson(id:str, out_path:str, prefix:str='biocjson_', wait:int|float=0)
     # Check if the request was successful
     if response.status_code == 200:
         # Return the response in JSON format
-        result = response.json() 
+        result = response.json()
         # light clean?
-        new_result = result['PubTator3'][0]
+        new_result = result["PubTator3"][0]
 
         # output to json
-        with open(os.path.join(out_path, f'{prefix}{id}.json'), 'w') as file:
+        with open(os.path.join(out_path, f"{prefix}{id}.json"), "w") as file:
             json.dump(new_result, file, indent=4)
-        
+
         return new_result
     else:
-        print(f"Unable to retrieve {id}: \n Error {response.status_code}: {response.text}")
+        print(
+            f"Unable to retrieve {id}: \n Error {response.status_code}: {response.text}"
+        )
 
     # add delay before next request
-    time.sleep(wait)        
+    time.sleep(wait)
 
 
-def passages_to_df(result:dict, out_path:str, prefix:str='df_')->pd.DataFrame:
+def passages_to_df(result: dict, out_path: str, prefix: str = "df_") -> pd.DataFrame:
     """
     Parses a BioC JSON result from get_biocjson() and returns a DataFrame of sentences with metadata.
 
@@ -82,60 +93,45 @@ def passages_to_df(result:dict, out_path:str, prefix:str='df_')->pd.DataFrame:
     ### PRECONDITIONS
     assert isinstance(result, dict), f"result must be a dict: {result}"
     assert_path(out_path)
-    assert isinstance(prefix, str), f'prefix must be a string: {prefix}'
-
+    assert isinstance(prefix, str), f"prefix must be a string: {prefix}"
 
     ### MAIN FUNCTION
     # get ids
-    id = result['pmid']
-    pmcid = result['pmcid']
+    id = result["pmid"]
+    pmcid = result["pmcid"]
 
     # columns
-    columns = ['index', 'section', 'sentence']
+    columns = ["index", "section", "sentence"]
 
     if pmcid is None:
-        pas  = [
-            [
-                i, 
-                x['infons']['type'], 
-                x['text']
-            ]
-            for i, x in enumerate(result['passages'])
+        pas = [
+            [i, x["infons"]["type"], x["text"]]
+            for i, x in enumerate(result["passages"])
         ]
 
     else:
-        pas  = [
-            [
-                i, 
-                x['infons']['section_type'], 
-                x['text']
-            ]
-            for i, x in enumerate(result['passages'])
+        pas = [
+            [i, x["infons"]["section_type"], x["text"]]
+            for i, x in enumerate(result["passages"])
         ]
 
     # put into a dataframe with metadata columns
     df = pd.DataFrame(pas, columns=columns)
-    df['pmid'] = id
-    df['pmcid'] = pmcid
-    df['date'] = result['date']
-    df['authors'] = ' and '.join(result['authors'])
-    df['journal'] = result['journal']
+    df["pmid"] = id
+    df["pmcid"] = pmcid
+    df["date"] = result["date"]
+    df["authors"] = " and ".join(result["authors"])
+    df["journal"] = result["journal"]
 
     # save to csv
-    df.to_csv(
-        os.path.join(out_path, f'{prefix}{id}.csv'), 
-        index=False,
-        sep='\t'
-    )
+    df.to_csv(os.path.join(out_path, f"{prefix}{id}.csv"), index=False, sep="\t")
 
     return df
 
 
 def collapse_sections(
-        df:pd.DataFrame, 
-        out_path:str, 
-        prefix:str='sectioned_'
-    )->pd.DataFrame:
+    df: pd.DataFrame, out_path: str, prefix: str = "sectioned_"
+) -> pd.DataFrame:
     """
     Given df from passages_to_df(), collapses the section into a single text block.
 
@@ -146,44 +142,53 @@ def collapse_sections(
 
     ### PRECONDITIONS
     # dtypes
-    assert isinstance(df, pd.DataFrame), f'df must be a dataframe: {df}'
+    assert isinstance(df, pd.DataFrame), f"df must be a dataframe: {df}"
     assert_path(out_path)
-    assert isinstance(prefix, str), \
-        f'prefix must be a string: {prefix}'
+    assert isinstance(prefix, str), f"prefix must be a string: {prefix}"
     # other
-    assert 'section' in df.columns, \
-        f'"section" column does not exist in df columns: {df.columns}'
-    assert 'index' in df.columns, \
-        f'"index" column does not exist in df columns: {df.columns}'
-    assert 'sentence' in df.columns, \
-        f'"sentence" column does not exist in df columns: {df.columns}'
-    assert 'pmid' in df.columns, \
-        f'"pmid" column does not exist in df columns: {df.columns}'
-    
+    assert (
+        "section" in df.columns
+    ), f'"section" column does not exist in df columns: {df.columns}'
+    assert (
+        "index" in df.columns
+    ), f'"index" column does not exist in df columns: {df.columns}'
+    assert (
+        "sentence" in df.columns
+    ), f'"sentence" column does not exist in df columns: {df.columns}'
+    assert (
+        "pmid" in df.columns
+    ), f'"pmid" column does not exist in df columns: {df.columns}'
+
     ### MAIN FUNCTION
 
     # get pmid
-    assert df['pmid'].nunique()==1, \
-        f'all pmids should be the same: {df["pmid"].unique()}'
-    id = df['pmid'].unique()[0]
+    assert (
+        df["pmid"].nunique() == 1
+    ), f'all pmids should be the same: {df["pmid"].unique()}'
+    id = df["pmid"].unique()[0]
 
     # init dict
     dict_dfs = {}
 
     # group by section
-    grouped = df.groupby('section')
+    grouped = df.groupby("section")
 
     for sec in grouped:
         # verbose
-        #print(sec[0])
+        # print(sec[0])
         # sentences in order
-        section_rows = sec[1].sort_values(by='index', ascending=True)
+        section_rows = sec[1].sort_values(by="index", ascending=True)
         # join the text
-        section_rows['text'] = (' '.join(section_rows['sentence']))
+        section_rows["text"] = " ".join(section_rows["sentence"])
         # remove rows (only need 1)
-        section_rows = section_rows.drop_duplicates(subset=['text', 'section', ])
+        section_rows = section_rows.drop_duplicates(
+            subset=[
+                "text",
+                "section",
+            ]
+        )
         # drop some cols
-        section_rows = section_rows.drop(['index', 'sentence'], axis=1)
+        section_rows = section_rows.drop(["index", "sentence"], axis=1)
         # reset index
         section_rows = section_rows.reset_index(drop=True)
         # add to dict
@@ -193,14 +198,14 @@ def collapse_sections(
     collapsed = pd.concat(dict_dfs, ignore_index=True)
 
     # save to csv
-    collapsed.to_csv(os.path.join(out_path, f'{prefix}{id}.csv'), index=False)
+    collapsed.to_csv(os.path.join(out_path, f"{prefix}{id}.csv"), index=False)
 
     return collapsed
 
 
-def get_smaller_texts(text:str, max_tokens:int)->list:
-    '''
-    Splits text up into smaller strings with tokens up to max_tokens, keeping sentences whole if possible. 
+def get_smaller_texts(text: str, max_tokens: int) -> list:
+    """
+    Splits text up into smaller strings with tokens up to max_tokens, keeping sentences whole if possible.
     Will split up sentences if max_tokens is really small.
 
     :param text: The larger string to break up into smaller substrings.
@@ -209,13 +214,13 @@ def get_smaller_texts(text:str, max_tokens:int)->list:
     :type max_tokens: int
     :returns: The smaller substrings as a list.
     :rtype: list
-    '''
+    """
 
     ### PRECONDITION CHECKS
     assert isinstance(text, str), f"text must be a string: {text}"
-    assert len(text)>0, f"text should not be an empty string: {text}"
+    assert len(text) > 0, f"text should not be an empty string: {text}"
     assert isinstance(max_tokens, int), f"max_tokens must be an integer: {max_tokens}"
-    assert max_tokens>0, f"max_tokens must not be 0: {max_tokens}"
+    assert max_tokens > 0, f"max_tokens must not be 0: {max_tokens}"
 
     ### MAIN FUNCTION
     # init
@@ -228,20 +233,21 @@ def get_smaller_texts(text:str, max_tokens:int)->list:
     # for each sentence
     for i, sent in enumerate(sentences):
         # check
-        assert len_text <= max_tokens, \
-            f'something went wrong, len_text longer than max_tokens: {i, len_text, max_tokens}'
+        assert (
+            len_text <= max_tokens
+        ), f"something went wrong, len_text longer than max_tokens: {i, len_text, max_tokens}"
 
         # get the number of tokens
         words = word_tokenize(sent)
         num_tokens = len(words)
 
-        if (len_text + num_tokens) <= max_tokens: 
-            # add the words to the 
+        if (len_text + num_tokens) <= max_tokens:
+            # add the words to the
             all_words += words.copy()
 
         else:
             # if not empty add words to the collection
-            if len_text>0:
+            if len_text > 0:
                 to_str = reverser.detokenize(all_words)
                 collection.append(to_str.strip())
 
@@ -252,19 +258,18 @@ def get_smaller_texts(text:str, max_tokens:int)->list:
                 broken_sentence = get_chunks(words, max_tokens)
 
                 for slice in broken_sentence:
-
                     # go from list of tokens back to str ..
-                    if len(slice)>0:
+                    if len(slice) > 0:
                         to_str = reverser.detokenize(slice)
                         collection.append(to_str.strip())
 
                 # reset
                 all_words = []
-        
+
         len_text = len(all_words)
 
     # adding whatever is left
-    if len_text>0:
+    if len_text > 0:
         to_str = reverser.detokenize(all_words)
         collection.append(to_str.strip())
 
@@ -273,13 +278,14 @@ def get_smaller_texts(text:str, max_tokens:int)->list:
     counter = 0
     for each in collection:
         counts = len(word_tokenize(each))
-        assert counts <= max_tokens, \
-            f'{counts} Sentence has more than {max_tokens} tokens: \n {each}'
+        assert (
+            counts <= max_tokens
+        ), f"{counts} Sentence has more than {max_tokens} tokens: \n {each}"
         counter += counts
     # checking that the num tokens organised in 'collection' are same as original
     original_count = len(word_tokenize(text))
-    assert abs(original_count-counter) < 3, \
-        f'token counts mismatched: {original_count, counter}'
+    assert (
+        abs(original_count - counter) < 3
+    ), f"token counts mismatched: {original_count, counter}"
 
     return collection
-
--- pubmed_rag/model.py
+++ pubmed_rag/model.py
@@ -1,40 +1,49 @@
 # imports
-import os, time, json, requests 
-from pubmed_rag.utils import assert_path, get_chunks
+import json
+import os
+import time
+
 import pandas as pd
-import transformers
+import requests
 import torch
 import torch.nn.functional as F
+import transformers
 
-#Mean Pooling - Take attention mask into account for correct averaging
+from pubmed_rag.utils import assert_path, get_chunks
+
+
+# Mean Pooling - Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask):
     """
     computes a single embedding (vector) for each sentence by averaging the token embeddings
     """
-    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    token_embeddings = model_output[
+        0
+    ]  # First element of model_output contains all token embeddings
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    )
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
 
 
 def get_tokens(
-        tokenizer:transformers.AutoTokenizer, 
-        input:list,
-        tokenizer_kwargs:dict=dict(
-            padding=True, 
-            truncation=True, 
-            return_tensors='pt',
-            max_length=512            
-        )
-        )->transformers.BatchEncoding:
+    tokenizer: transformers.AutoTokenizer,
+    input: list,
+    tokenizer_kwargs: dict = dict(
+        padding=True, truncation=True, return_tensors="pt", max_length=512
+    ),
+) -> transformers.BatchEncoding:
     """
     Uses a tokenizer for a given model from Hugging Face to encode a list of sentences.
 
     :param tokenizer: Tokenizer for a given model loaded from Hugging Face
     :type tokenizer: A transformers class
-    :param input: A list of sentences to be embedded 
+    :param input: A list of sentences to be embedded
     :param type: list
     :param tokenizer_kwargs: Additional parameters to pass for
-    
+
     :returns: The encoded inputs that can be used as a dictionary.
     :rtype: transformers.BatchEncoding
     """
@@ -44,57 +53,53 @@ def get_tokens(
     #     f"Tokenizer must be from transformers: {type(tokenizer)}"
     assert isinstance(input, list), f"input must be a list of strings {input}"
     for item in input:
-        assert isinstance(item, str), \
-            f"input must be a list of strings: {item} is not str"
-    assert isinstance(tokenizer_kwargs, dict), \
-        f"tokenizer_kwargs must be a dict: {tokenizer_kwargs}"    
-    
+        assert isinstance(
+            item, str
+        ), f"input must be a list of strings: {item} is not str"
+    assert isinstance(
+        tokenizer_kwargs, dict
+    ), f"tokenizer_kwargs must be a dict: {tokenizer_kwargs}"
+
     # MAIN FUNCTION
 
     # get tokens
-    encoded_input = tokenizer(
-        input, 
-        **tokenizer_kwargs
-    ) 
+    encoded_input = tokenizer(input, **tokenizer_kwargs)
 
     return encoded_input
 
 
 def get_sentence_embeddings(
-        model:transformers.AutoModel, 
-        encoded_input:transformers.BatchEncoding,
-        )->torch.Tensor:
+    model: transformers.AutoModel,
+    encoded_input: transformers.BatchEncoding,
+) -> torch.Tensor:
     """
     Uses a given model from Hugging Face to embed a list of sentences
     that have been encoded by pubmed_rag.model.get_tokens()
 
     :param model: The model loaded from Hugging Face
     :type model: transformers.AutoModel
-    :param encoded_input: A list of sentences to be embedded 
+    :param encoded_input: A list of sentences to be embedded
     :type encoded_input: transformers.BatchEncoding
-    
+
     :returns: The embeddings
     :rtype: torch.Tensor
     """
 
     # PRECONDITION CHECKS
-    assert isinstance(encoded_input, transformers.BatchEncoding), \
-        f"tokens must be a transformers.BatchEncoding: {encoded_input}"
+    assert isinstance(
+        encoded_input, transformers.BatchEncoding
+    ), f"tokens must be a transformers.BatchEncoding: {encoded_input}"
 
     # MAIN FUNCTION
 
     # Compute token embeddings
     with torch.no_grad():
-        model_output = model(**encoded_input)      
+        model_output = model(**encoded_input)
 
     # Perform pooling
-    sentence_embeddings = mean_pooling(
-        model_output, 
-        encoded_input['attention_mask']
-    )
+    sentence_embeddings = mean_pooling(model_output, encoded_input["attention_mask"])
 
     # Normalize embeddings
-    sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)   
+    sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
 
-    return sentence_embeddings 
-
+    return sentence_embeddings
